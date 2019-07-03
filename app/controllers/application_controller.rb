c5edc5bda548f57d03b0b0bfc55dbbaca1734a4b
DrewE <andrew.engelstein@gmail.com>
2019-07-03 13:36:23 -0700

    Remove Devise::Registrations controller and fix original issues by updating ApplicationController to respond_to :json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
-
+  respond_to :json
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user
 
--- test/controllers/devise/registrations_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Devise::RegistrationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
